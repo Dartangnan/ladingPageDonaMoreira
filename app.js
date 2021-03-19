8593a1b2c0bc9037ab356aae282fd2ecf2b049f6
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-19 06:49:21 -0700

    make nav bar html/css

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// imports:
 const express = require("express");
 
+// creating express app:
 const app = express();
 
+// GET request to the home page:
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// App listening to port 3000:
 app.listen(3000, function (err) {
   if (err) {
     console.log(err);
